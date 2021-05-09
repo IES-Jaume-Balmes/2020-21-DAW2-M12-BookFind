637c31852ee484befad55a2d36ad2067f5535580
barbara <herrerabarbaraf@gmail.com>
2021-05-09 23:40:22 +0200

    vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     themeConfig: {
-  /*      base: '/<publication-repo-name>/',*/
+        base: '/',
+        deploy: "npm run build && npx gh-pages docs/.vuepress/dist",
         logo: '/img/logo.png',
         sidebar: [
             {
