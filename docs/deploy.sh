35ac43e02f0e4c61fbf3a6ed6ea6e3e4310749fe
barbara <herrerabarbaraf@gmail.com>
2021-05-10 12:01:14 +0200

    vuepress no funciona

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -21,5 +21,5 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:barbarahf/IES-Jaume-Balmes.git main:https://ies-jaume-balmes.github.io/2020-21-DAW2-M12-BookFind/
+git push -f git@github.com/IES-Jaume-Balmes/2020-21-DAW2-M12-BookFind.git main:gh-pages
 cd -
