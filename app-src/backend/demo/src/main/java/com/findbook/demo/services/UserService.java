904197da379e0ecaa42d1990fbcfdb2d98bd6f1b
barbara herrers <herrerabarbaraf@gmail.com>
2021-05-18 18:07:48 +0200

    register ok

--- app-src/backend/demo/src/main/java/com/findbook/demo/services/UserService.java
+++ app-src/backend/demo/src/main/java/com/findbook/demo/services/UserService.java
@@ -8,21 +8,16 @@ import com.findbook.demo.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class UserService  implements UserDetailsService {
+public class UserService   {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    ConfirmationTokenService confirmationTokenService;
-    @Autowired
-    EmailSenderService emailSenderService;
+
 /*    @Autowired
     private PasswordEncoder passwordEncoder;*/
 
@@ -40,8 +35,6 @@ public class UserService  implements UserDetailsService {
         user.setCart(userCart);
         user.setRol(Rol.ROLE_USER);
 
-        final ConfirmationToken confirmationToken = new ConfirmationToken(user);
-        confirmationTokenService.saveConfirmationToken(confirmationToken);
 
 
         userRepository.save(user);
@@ -49,23 +42,6 @@ public class UserService  implements UserDetailsService {
 
     }
 
-    void sendConfirmationMail(String userMail, String token) {
-
-        final SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(userMail);
-        mailMessage.setSubject("Mail Confirmation Link!");
-        mailMessage.setFrom("<MAIL>");
-        mailMessage.setText(
-                "Thank you for registering. Please click on the below link to activate your account." + "http://localhost:8080/sign-up/confirm?token="
-                        + token);
-
-        emailSenderService.sendEmail(mailMessage);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
 
     //Login user
 
