832b5764a9ecd7938803063445ab2eaace4ef704
barbara herrers <herrerabarbaraf@gmail.com>
2021-05-28 19:33:49 +0200

    paypal beans

---
+++ app-src/backend/demo/src/main/java/com/findbook/demo/controllers/PaypalController.java
@@ -0,0 +1,9 @@
+package com.findbook.demo.controllers;
+
+import com.findbook.demo.services.PaypalService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PaypalController {
+    PaypalService service;
+}
